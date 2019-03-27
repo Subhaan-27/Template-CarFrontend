09267ab3b31f2f66738a980a65891c115ab8c24c
Subhaan-27 <36763210+Subhaan-27@users.noreply.github.com>
2019-03-27 13:55:29 +0100

    post og get virker

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,7 +7,47 @@ import { ICar } from "./Icar";
 
 let ContentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content")
 let GetAllCarsButton: HTMLButtonElement =<HTMLButtonElement> document.getElementById("getAllButton")
+
 GetAllCarsButton.addEventListener('click', ShowAllCars)
+
+let AddCarButton: HTMLButtonElement =<HTMLButtonElement> document.getElementById("addButton")
+AddCarButton.addEventListener('click', addCar);
+
+function addCar(): void{ 
+
+    let addModelelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addModel")
+    let addVendorelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addVendor")
+    let addPriceelement: HTMLInputElement = <HTMLInputElement> document.getElementById("addPrice")
+
+    let myModel: string = addModelelement.value;
+    let myVendor: string = addVendorelement.value;
+    let myPrice: number = +addPriceelement.value;
+    console.log("model" + myModel);
+    console.log ("vendor"+ myVendor);
+    console.log ("price"+ myPrice);
+
+
+
+    axios.post<ICar>("https://webapicar20190326034339.azurewebsites.net/api/cars", 
+    {model:myModel, vendor:myVendor, price:myPrice}) 
+    .then (function (response:AxiosResponse): void
+    {
+        console.log("Statuskoden er :" +response.status);
+    })
+    .catch(
+        function (error:AxiosError) :void
+        {
+            console.log("Error i min kode")
+            console.log(error)
+
+        }
+        )
+    
+    
+
+
+}
+
 
 function ShowAllCars():void{
     axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
