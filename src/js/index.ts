6a9dedce73ed32a6c62f7d11db7a49f0f5523e68
Subhaan-27 <36763210+Subhaan-27@users.noreply.github.com>
2019-03-27 11:34:43 +0100

    get

--- src/js/index.ts
+++ src/js/index.ts
@@ -11,16 +11,18 @@ function ShowAllCars():void{
     axios.get<ICar[]>("https://webapicar20190326034339.azurewebsites.net/api/cars")
     .then(function(response:AxiosResponse<ICar[]>): void
     {
+        console.log("Er i Then")
         console.log(response)
-
+        
     })
     .catch(
         function(error:AxiosError) : void{
+            console.log("Error i min kode")
             console.log(error)
 
         }
         )
-    
+        console.log("Er i slutningen af ShowAllCars function")
 
 }
 
